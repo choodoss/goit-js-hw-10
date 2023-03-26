37690111d3c3cba71d6d6ebbd6dc0188b93b31ee
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2023-03-26 20:25:59 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-// import fetchCountries from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { createHtml, createListHtml } from './js/constructor'
 const DEBOUNCE_DELAY = 300;
-
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages,translations&lang=uk`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        }).catch(err => console.log(err))
-};
 
 const searchEl = document.querySelector('#search-box');
 const countryInfoEl = document.querySelector('.country-info');
+const countryssearchEl = document.querySelector('.country-list');
 
 function handleSearchCountry(e) {
-    let name = e.target.value;
-    console.log(name)
+    let name = e.target.value.trim();
+    if (!name) {
+        return Notify.failure('В інпуті немає даних, для пошуку вкажіть предмет пошуку в інпуті');
+    }
+    fetchCountries(name)
+        .then(data => {
+            console.log(data.length)
+            if (data.length > 1 || data.length < 10) { // якщо в масиві більше 1 елементу, оновлюємо список країн
+                countryssearchEl.innerHTML = createListHtml(data);
+                countryInfoEl.innerHTML = ''; // очистити інформацію про країну
+                console.log(data.length > 1)
+            } else { // якщо в масиві всього 1 елемент, оновлюємо інформацію про країну
+                countryInfoEl.innerHTML = createHtml(data);
+                countryssearchEl.innerHTML = ''; // очистити список країн
+            }
+        })
+        .catch(() => Notify.failure('Error 404', 'Такої країни не знайдено!', 'Зрозуміло'));
+    console.log(name);
 }
 
 searchEl.addEventListener('input', debounce(handleSearchCountry, 300));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,10 @@
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}`)
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages,translations&lang=uk`)
         .then(response => {
             if (!response.ok) {
-                console.error();
                 throw new Error(response.status);
             }
-            console.log(response)
-            console.log(response.json())
             return response.json();
         })
-        .catch(error => console.log(error));
 };
+//
